38686bf8346766630ee47e2651b8283ce08cc748
Priyansha29 <priyanshagour29@gmail.com>
2025-07-24 01:13:50 +0530

    Create Expt6E.cpp

---
+++ Expt6E.cpp
@@ -0,0 +1,16 @@
+//Priyansha
+//24070123079
+//A3
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter 5 digits of prn: ";
+    cin>>a;
+    while (al = 0) {
+        b = a * 10
+        a = a / 10
+        cout<<b<<endl;
+    }
+return 0;
+}
