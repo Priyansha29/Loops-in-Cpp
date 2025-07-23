e463a476ed54844cce3a4ec2a7f59378dd985309
Priyansha29 <priyanshagour29@gmail.com>
2025-07-24 01:11:49 +0530

    Create Expt6D.cpp

---
+++ Expt6D.cpp
@@ -0,0 +1,22 @@
+//Priyansha
+//24070123079
+//ENTC-A3
+#include <iostream
+using namespace std;
+int main(){
+    int password-123,
+    int user password;
+    cout"Enter the password":
+    cinuser password:
+    while(password=0){
+      if (password user password){
+          cout Incorrect password \n":
+          cout Enter password in";
+          cinuser password;
+      }
+      else{
+          coutsystem is unlocked"<<endl;
+          break;
+}
+return 0;
+      }
