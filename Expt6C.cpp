62866eff6b668bb04eedcbef2486c53e46c3bad3
Priyansha29 <priyanshagour29@gmail.com>
2025-07-24 01:08:28 +0530

    Create Expt6C.cpp

---
+++ Expt6C.cpp
@@ -0,0 +1,14 @@
+//Priyansha
+//24070123079
+//ENTC-A3
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    while i < 20 )\
+        cout<<1+1<<"";
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
