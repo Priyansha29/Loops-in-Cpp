dc5f038bc2b113001d5d346e7dc3c18b5e2ccb29
Priyansha29 <priyanshagour29@gmail.com>
2025-07-24 01:04:27 +0530

    Create Expt6B.cpp

---
+++ Expt6B.cpp
@@ -0,0 +1,13 @@
+//Priyansha
+//24070123079
+//ENTC-A3
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    for(i=0;i<5;i++){
+        cout<<"SIT \n";
+    }
+
+    return 0;
+}
