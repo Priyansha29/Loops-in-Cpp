b44b5373c74c3141ba11657f8baea1fb3df95cb7
Priyansha29 <priyanshagour29@gmail.com>
2025-07-24 02:15:07 -0400

    Create Expt6F.cpp

---
+++ Expt6F.cpp
@@ -0,0 +1,16 @@
+//Priyansha
+//24070123079
+//ENTC-A3
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1;i<=2;++i){
+    cout<<"Outer:"<<i<<"\n";
+    
+    for(int j=0;j<=3;++j){
+        cout<<"Inner:"<<j<<"\n";
+    }
+    }
+    return 0;
+}
