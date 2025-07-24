824e91f67222d403a7c3b3d7c6d4aa64e152d9b0
Priyansha29 <priyanshagour29@gmail.com>
2025-07-24 02:18:08 -0400

    Create Expt6I.cpp

---
+++ Expt6I.cpp
@@ -0,0 +1,20 @@
+//PRIYANSHA GOUR
+//24070123079
+//ENTC-A3
+#include <iostream>
+using namespace std;
+int main() {
+    int i, j, k;
+    int n=1;
+    int a=4;
+    for(i=1;i<=a;i++){
+        for(j=1;j<=(i);j++){
+            cout<<n;
+            n++;
+        }
+            
+        cout<<endl;
+    }
+
+    return 0;
+}
